9fb8e65e25deeb0373e88be8bbc14f54b9fca827
Will Germany <germany.william@gmail.com>
2025-11-11 20:01:47 -0500

    feat: Use analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
 		<html lang="en" className={`${inter.variable} ${spaceGrotesk.variable}`}>
 			<body>
 				<StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+				<Analytics />
 			</body>
 		</html>
 	);
