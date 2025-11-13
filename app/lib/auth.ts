f3c3b5a69456db24054eaf88de8c801521f12123
Will Germany <germany.william@gmail.com>
2025-11-13 17:52:15 -0500

    fix: build issue

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 import { magicLinkClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
-	baseURL: "/api/auth",
+	basePath: "/api/auth",
 	plugins: [magicLinkClient()],
 });
 
