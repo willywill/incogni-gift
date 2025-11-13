8cbbe88fecca5085d11500583eb014cdfc4a21b9
Will Germany <germany.william@gmail.com>
2025-11-13 17:05:02 -0500

    fix: CORS error

--- next.config.js
+++ next.config.js
@@ -9,6 +9,31 @@ const nextConfig = {
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
+	async headers() {
+		return [
+			{
+				source: "/api/:path*",
+				headers: [
+					{
+						key: "Access-Control-Allow-Origin",
+						value: "https://www.incognigift.com",
+					},
+					{
+						key: "Access-Control-Allow-Methods",
+						value: "GET, POST, PUT, DELETE, OPTIONS",
+					},
+					{
+						key: "Access-Control-Allow-Headers",
+						value: "Content-Type, Authorization, Cookie",
+					},
+					{
+						key: "Access-Control-Allow-Credentials",
+						value: "true",
+					},
+				],
+			},
+		];
+	},
 };
 
 module.exports = nextConfig;
